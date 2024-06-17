18f131f67d15944028f9ef02fda7bd81996cb5a1
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-06-17 07:37:06 +0530

    implement clerk webhooks

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose, {Mongoose} from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL ;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection{
     conn : Mongoose | null ;
@@ -27,4 +27,4 @@ export const connectToDatabase = async () => {
     cached.conn = await cached.promise;
 
     return cached.conn;
-}
+};
